d1310ebd4d9a45b558aa81a259f9dd10800fe27e
alescho <ale2053972@maricopa.edu>
2026-04-22 11:47:14 -0700

    creating loop for guesses

--- guess_the_word.py
+++ guess_the_word.py
@@ -2,14 +2,19 @@
 # CIS256 Term Year Spring 2026
 # Exercise Assignment 4
 
+#import random for random word selection
 import random
 
+#creating game function
 def game():
-    word = ['moose', 'mountain', 'horse', 'tiger', 'lamborghini', 'machine']
+    word = ['moose', 'mountain', 'horse', 'tiger', 'lamborghini', 'machine'] 
     word_selection = random.choice(word)
     display = ['_'] * len(word_selection)
     guesses = []
     attempts = 10
     
+    #printing game name for user
     print('Play guess the word!')
-    
+
+    while attempts > 0 and '_' in display: 
+        print
