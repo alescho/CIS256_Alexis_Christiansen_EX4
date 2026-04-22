00c7723bc0ac85f3b5144d19b62b0385cb5e94ed
alescho <ale2053972@maricopa.edu>
2026-04-22 11:36:35 -0700

    creating program

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,3 +1,15 @@
 # Alexis Christiansen
 # CIS256 Term Year Spring 2026
 # Exercise Assignment 4
+
+import random
+
+def game():
+    word = ['moose', 'mountain', 'horse', 'tiger', 'lamborghini', 'machine']
+    word_selection = random.choice(word)
+    display = ['_'] * len(word_selection)
+    guesses = []
+    attempts = 10
+    
+    print('Play guess the word!')
+    
